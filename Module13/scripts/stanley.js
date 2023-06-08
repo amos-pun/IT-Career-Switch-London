07f22cdc2441d9f6b0a3ff8b0a8c71b0d4ffbe61
amos-pun <itsamospun@gmail.com>
2023-06-08 13:58:22 +0100

    decimal and zero bug has been debuged

--- Module13/scripts/stanley.js
+++ Module13/scripts/stanley.js
@@ -1,6 +1,7 @@
-function equivalentCheck(){ //This allows the last value to remain in view untill other buttons are pressed.
+function equivalentCheck(){ //This allows the last value to remain in view until   other buttons are pressed.
     console.log("equivalentCheck()");
-    if(parseInt(document.getElementById('equivalent').value)){
+    if 
+    (parseInt(document.getElementById('equivalent').value)){
         document.getElementById('equivalent').value = 0;
         document.getElementById('result').value = 0;
     }
@@ -11,12 +12,9 @@ function input(x) {
     equivalentCheck();
 
     let y = parseFloat(document.getElementById('result').value);
-    console.log("-----y");
-    console.log(y)
-    console.log("-----yy");
 
     if(document.getElementById('decimalVar').value == 0){
-        x += y * 10; //multiply the text input by 10 and add the value of x.
+         x += y * 10; //multiply the text input by 10 and add the value of x.
 
         document.getElementById('result').value = x; //Return x to the text output.
         
@@ -33,17 +31,18 @@ function input(x) {
         else{
             document.getElementById('result').value += x;
         }
+         
 
+        //the lines below are unnecessary
         //decimalCount++;
         //document.getElementById('decimalVar').value = decimalCount;
     }
 
 
-   // if (document.getElementById("result").value == 0){ //This check the zero and gets rid of it when we enter number fo the first time.
+   /* if (document.getElementById("result").value == 0){ //This check the zero and gets rid of it when we enter number fo the first time.
     
-   //     document.getElementById('result').value = x;
-
-   // }  
+        document.getElementById('result').value = x;
+    }   */
 }
 
 function decimalPoint(){
@@ -52,20 +51,21 @@ function decimalPoint(){
     }
     /*
     if(parseInt(document.getElementById('operation').value)){ //If this is an empty string it will return false.
-        document.getElementById('result').value = 0;
+        document.getElementById('result').value = "";
     }
     */
 }
 
 function operandCheck() {
+    //check if there is the first number and store it in memory
     if(document.getElementById('operand').value == "") {
+        //if we did not save it save what is on the screen as th first number
         document.getElementById('operand').value = document.getElementById('result').value;
-
-        document.getElementById('result').value = 0; //this is temporary.
-       // document.getElementById('equivalent').value = 1;
+        //document.getElementById('result').value = 0; //this is temporary.
+        document.getElementById('equivalent').value = 1;
     }
     else{
-
+        //if we have the first number in memory then check the operation to be done 
         operatorCheck();
     }
 }
@@ -102,7 +102,7 @@ function operatorCheck(){
 
 
 function operators(x){
-    
+
     switch (x) {
         case 1:
             document.getElementById('operation').value = 1; //addition
@@ -119,7 +119,7 @@ function operators(x){
         default:
     }
 
-    document.getElementById('decimalVar').value=0;
+    document.getElementById('decimalVar').value=0; //added this line
     operandCheck();
     
 }
@@ -130,38 +130,42 @@ function equals() {
     document.getElementById('result').value = document.getElementById('operand').value;
     document.getElementById('operand').value = "";
     document.getElementById('equivalent').value = 1;
-    document.getElementById('decimalVar').value = 0; // added line
+    document.getElementById('decimalVar').value=0;  //added this line
     
 }
 
 function allclear() {
+   //perfect
     document.getElementById('result').value = 0;
     document.getElementById('operand').value = "";
     document.getElementById('operation').value = 0;
     document.getElementById('equivalent').value = 0;
-    document.getElementById('decimalVar').value = 0 // added line
+    document.getElementById('decimalVar').value=0; //added this line
 }
 
 function plusminus() {
+    //perfect
     let x = parseFloat(document.getElementById('result').value)
 
-    x *= -1;
+    x *= -1; //make it negative or positive
 
     document.getElementById('result').value = x;
 }
 
 function percent() {
+    //perfect
     let x = parseFloat(document.getElementById('result').value)
 
-    x *= 0.01;
+    x *= 0.01;  // this is the same as x/100
 
     document.getElementById('result').value = x;
 }
 
 function square() {
+    //perfect
     let x = parseFloat(document.getElementById('result').value)
 
-    x *= x;
+    x *= x;   // thisa is x times x
 
     document.getElementById('result').value = x;
 }
